254e0c6bb4ca0ec7d296b1b33b7b4bf0cbb88e69
Caleb Holdaway <cdholdaway@gmail.com>
2023-04-11 19:38:21 -0600

    Shuffle and Manual testing Updated

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,21 @@
-const { shuffle } = require('../src/shuffle')
+const shuffle = require('../src/shuffle');
+
+const argument = ["Caleb", "Alex", "Jordan"];
 
 describe('shuffle should...', () => {
-  // CODE HERE
-})
+  it("return an array", () => {
+    const result = shuffle()
+    expect(result.length).toBe(0);
+  });
+
+  it("return an array of the same length as the argument sent in", () => {
+    const result = shuffle(argument);
+    expect(argument.length).toBe(result.length);
+  });
+
+  it("check that all the same items are in the array", () => {
+    const result = shuffle(argument);
+    const isSame = argument.sort().join(',') === result.sort().join(',')
+    expect(isSame).toBe(true)
+  })
+});
